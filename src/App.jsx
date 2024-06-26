c74c4aed1587f7ee8e0dd5f2fab893a28389eb9c
Tabishkhan18 <tabishkhan1811@gmail.com>
2024-06-26 16:15:59 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,25 @@ import Footer from './components/Footer';
 const slides = [
   {
     logo: 'accenture.png',
-    text: 'Everyone loves it; it has democratized our finance function. In some ways Trello shattered hierarchy and brought us together.',
+    text: 'Everyone loves it; it has democratized our finance function. In some ways Trello shattered hierarchy.',
     name: 'THOR',
     position: 'Founder, Accenture',
   },
   {
     logo: 'amazon.png',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eveniet distinctio earum ullam. Officiis ut necessitatibus facere.',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eveniet distinctio earum ullam. Officii.',
     name: 'STEVE ROGERS',
     position: 'Finance, Amazon',
   },
   {
     logo: 'americanexpress.png',
-    text: 'Everyone loves it; it has democratized our finance function. In some ways Trello shattered hierarchy and brought us together.',
+    text: 'Everyone loves it; it has democratized our finance function. In some ways Trello shattered hierarchy.',
     name: 'BRUCE BANNER',
     position: 'CEO, American Express',
   },
   {
     logo: 'microsoft.png',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eveniet distinctio earum ullam. Officiis ut necessitatibus facere.',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis eveniet distinctio earum ullam.',
     name: 'TONY STARK',
     position: 'Director, Microsoft',
   },
